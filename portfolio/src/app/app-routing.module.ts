54820d1148e4fe79af55c1f1c6552deda6976206
f.aranzulla <f.aranzulla@oneoffsolution.it>
2024-05-14 17:40:41 +0200

    add hero message and buttons

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contacts', component: ContactsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
