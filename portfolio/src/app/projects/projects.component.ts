b5d90b34049899014917b70943f4fa6d4765ce2a
f.aranzulla <f.aranzulla@oneoffsolution.it>
2024-05-17 17:52:58 +0200

    ADD: add projects

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -7,9 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
-  urlProjects: string[] = [
-    '../../assets/img/sassoCartaForbice.png'
-  ]
+  projects: {
+    title: string, 
+    img: string, 
+    url: string, 
+    technologies: string[],
+    info: string 
+  }[] = [
+    { title: 'Sasso carta forbice', img: '../../assets/img/sassoCartaForbice.png', url: 'https://fabioaranzulla.github.io/sasso-carta-forbice/', technologies: ['HTML',' CSS',' Javascript'], info: 'sassoCartaForbice' },
+    { title: 'Calcolatrice Apple', img: '../../assets/img/calcolatriceApple.png', url: 'https://fabioaranzulla.github.io/Calcolatrice-Apple/', technologies: ['HTML',' CSS',' Javascript'], info: 'calcolatriceApple' },
+    { title: 'App meteo', img: '../../assets/img/appMeteo.png', url: 'https://fabioaranzulla.github.io/WeatherApp/', technologies: ['React',' Tailwind '], info: 'appMeteo' },
+    { title: 'Netflix', img: '../../assets/img/netflix.png', url: '#', technologies: ['Angular',' Bootstrap ', ' Typescript'], info: 'netflix' },
+    ];
 
   constructor(){}
+
 }
